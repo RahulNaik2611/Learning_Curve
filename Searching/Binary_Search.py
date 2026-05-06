3ff208840471d7a7470aefee678c7f2e5159f9c5
B Rahul Naik <banavathrahulnaik26@gmail.com>
2026-05-06 20:17:40 +0530

    File handling (Learning)

---
+++ Searching/Binary_Search.py
@@ -0,0 +1,19 @@
+def binary_searching(arr, target):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+l = [17,18,19,20]
+target = 19
+print(binary_searching(l, target))
+
+
+
